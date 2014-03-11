47b202f34e4a438e7f6435ed5945e4076d295026
David Dionisio <garapata@gmail.com>
2014-03-10 19:51:34 -0700

    more ending stuff

---
+++ Assets/Scripts/EndComputeClearTime.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndComputeClearTime : MonoBehaviour {
+    public UILabel label;
+
+    public void RefreshClearTime() {
+    }
+}
---
+++ Assets/Scripts/SceneSkip.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSkip : MonoBehaviour {
+    public float wait = 1.5f;
+    public string toScene;
+
+    void OnDestroy() {
+        InputManager input = Main.instance ? Main.instance.input : null;
+        if(input) {
+            input.RemoveButtonCall(0, InputAction.MenuEscape, OnInput);
+        }
+    }
+
+    void Awake() {
+        Invoke("EnableInput", wait);
+    }
+
+    void EnableInput() {
+        Main.instance.input.AddButtonCall(0, InputAction.MenuEscape, OnInput);
+    }
+
+    void OnInput(InputManager.Info dat) {
+        if(dat.state == InputManager.State.Pressed) {
+            UIModalConfirm.Open(GameLocalize.GetText("skip_confirm_title"), GameLocalize.GetText("skip_confirm_desc"),
+                                delegate(bool yes) {
+                if(yes) {
+                    Main.instance.sceneManager.LoadScene(toScene);
+                }
+            });
+        }
+    }
+}
